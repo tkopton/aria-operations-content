675ea9477a6f5cd357dd8cdfd2c21ca16355013b
Thomas Kopton <tkopton@gmail.com>
2025-02-24 13:43:24 +0100

    Update getSSLRemainigDays.sh

--- SSLCheckFiles/getSSLRemainigDays.sh
+++ SSLCheckFiles/getSSLRemainigDays.sh
@@ -19,9 +19,9 @@ datediff() {
   fi
 }
 
-if [ $# -eq 0 ]; then
-  display_usage
-  exit 1
+if (( $# == 0 )); then
+    display_usage
+    exit 1
 fi
 
 cert_date=$(echo | openssl s_client -servername $1 -connect $1:$2 2>/dev/null | openssl x509 -noout -dates | grep notAfter | cut -d= -f2)
